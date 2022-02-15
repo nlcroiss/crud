01f3fde2f2d13d4d75c8b3bc0d6505c67fe77409
neldo <nlcroiss@gmail.com>
2022-02-14 23:25:34 -0300

    primera version del proyecto

---
+++ temp/encabezado.php
@@ -0,0 +1,79 @@
+<header class="encabezado">
+      <!-- TODO: TÍTULO PRINCIPAL DE LA PÁGINA -->
+
+      <div id="principalFondo">
+        <img
+          class="imagenTitulo"
+          src="img/blur.png"
+          alt="logo del título principal"
+        />
+        <h1 id="principalTitulo">Tecnología Educativa</h1>
+      </div>
+
+      <!-- TODO: bARRA DE MENÚ PRINCIPAL-->
+
+      <nav class="navbar navbar-expand-md navbar-dark">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.html"
+                  >Inicio</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="programacion.html">Programación</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="robotica.html">Robótica</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="impresion.html">Impresión 3D</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="iot.html">IoT</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contacto.html">Contacto</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <!-- TODO: SLIDE CON TEXTO SOBRE EL ENCABEZADO DE LA PÁGINA -->
+      <div id="slideTexto" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="carousel-caption">
+              <div>
+                “No podemos abandonar los medios y las tecnologías digitales en
+                educación y volver a un tiempo más simple y más natural”.
+                (Buckingham).
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="carousel-caption">
+              <div>
+                "Los usuarios o consumidores de tecnología o medios digitales
+                están llamados a la creación, son los productores de sus propios
+                contenidos digitales." (Dussel y Quevedo)
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
